979635abf90414659a4b4ce151e30980d07d132a
alanborges69 <alan176966@gmail.com>
2017-03-15 22:27:37 -0300

    Cliente pra vários Pedidos e  Cliente pra um endereço

--- Cliente.java
+++ Cliente.java
@@ -1,6 +1,7 @@
 package br.edu.faculdadedelta.modelo;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -11,6 +12,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -31,7 +34,13 @@ public class Cliente extends BaseEntity<Long> {
 	@ManyToOne(cascade= {CascadeType.PERSIST}, fetch=FetchType.LAZY)
 	@JoinColumn(name="id_profissao", referencedColumnName="id_profissao",
 	   insertable=true, updatable=false, nullable=false)
-	private Profissao profisao;
+	private Profissao id_profisao;
+	
+	@OneToOne(mappedBy="Cliente", fetch=FetchType.LAZY )
+	private Endereco id_endereco;
+	
+	@OneToMany(mappedBy="Cliente", fetch=FetchType.LAZY)
+	private List<Pedido> compras;
 	
 	@Column(length=60, nullable=false)
 	private String nomeCliente;
@@ -46,10 +55,30 @@ public class Cliente extends BaseEntity<Long> {
 	}
 	
 	
-	public Cliente(Long id, Profissao profisao, String nomeCliente,
+	public Profissao getId_profisao() {
+		return id_profisao;
+	}
+
+
+	public void setId_profisao(Profissao id_profisao) {
+		this.id_profisao = id_profisao;
+	}
+
+
+	public Endereco getId_endereco() {
+		return id_endereco;
+	}
+
+
+	public void setId_endereco(Endereco id_endereco) {
+		this.id_endereco = id_endereco;
+	}
+
+
+	public Cliente(Long id, Profissao id_profisao, String nomeCliente,
 			String cpfCliente, Date dataNascimento) {
 		this.id = id;
-		this.profisao = profisao;
+		this.id_profisao = id_profisao;
 		this.nomeCliente = nomeCliente;
 		this.cpfCliente = cpfCliente;
 		this.dataNascimento = dataNascimento;
@@ -62,11 +91,11 @@ public class Cliente extends BaseEntity<Long> {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public Profissao getprofisao() {
-		return profisao;
+	public Profissao getid_profisao() {
+		return id_profisao;
 	}
-	public void setprofisao(Profissao profisao) {
-		this.profisao = profisao;
+	public void setid_profisao(Profissao id_profisao) {
+		this.id_profisao = id_profisao;
 	}
 	public String getNomeCliente() {
 		return nomeCliente;
